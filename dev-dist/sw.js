25f19abc2ae853b13fecb4d405e9ecdcf88dae6b
pedrofosabril <pedrofosabril@iico.edu.ar>
2026-09-08 10:09:05 -0300

    dependecias corregidas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4a46mllghbs"
+    "revision": "0.b248qnrlrmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
